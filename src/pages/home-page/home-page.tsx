895c93b83841b7e08136e9149c716d6839a22d6a
Lyubov <luartal@mail.ru>
2026-09-15 19:55:38 +0300

    style: add picture

--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -1,12 +1,18 @@
-import styles from './home-page.module.css';
+import styles from './home-page.module.css'
+import englishImg from '../../assets/img/english.jpg'
 
 const HomePage = () => {
-	return (
-		<div className={styles.home}>
-			<h1>CardTalk</h1>
-			<p>Приложение для изучения иностранных слов. Карточки, перевод, профиль.</p>
-		</div>
-	);
-};
+  return (
+    <div className={styles.home}>
+      <img
+        className={styles.image}
+        src={englishImg}
+        alt="English"
+      />
+      <h1>CardTalk</h1>
+      <p>Приложение для изучения иностранных слов. Карточки, авторитеты, приоритеты.</p>
+    </div>
+  )
+}
 
-export default HomePage;
+export default HomePage

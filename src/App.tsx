e2125fb29f4ed64547084c85a773a362eb705acf
Lyubov <luartal@mail.ru>
2026-09-15 15:11:25 +0300

    feat: add routing and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { store } from './store';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
 import { Body } from './components/body/body';
 import { HomePage } from './pages/home-page/home-page';
+import { LoginPage } from './pages/login-page/login-page';
+import { CardsPage } from './pages/cards-page/cards-page';
+import { ProfilePage } from './pages/profile-page/profile-page';
 
 export default function App() {
 	return (
 		<Provider store={store}>
 			<BrowserRouter>
-				<Header />
+				<Header
+					links={[
+						{ to: '/cards', label: 'Карточки' },
+						{ to: '/login', label: 'Войти' },
+					]}
+				/>
 				<Body>
-					<HomePage />
+					<Routes>
+						<Route path="/" element={<HomePage />} />
+						<Route path="/login" element={<LoginPage />} />
+						<Route path="/cards" element={<CardsPage />} />
+						<Route path="/profile" element={<ProfilePage />} />
+					</Routes>
 				</Body>
 				<Footer contacts={{ email: 'mail@cardtalk.ru', phone: '+7 999 123-45-67' }} />
 			</BrowserRouter>
---
+++ src/pages/cards-page/cards-page.tsx
@@ -0,0 +1,3 @@
+export const CardsPage = () => {
+	return <div>CardsPage</div>;
+};

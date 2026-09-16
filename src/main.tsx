f8f2592f1e35a1a8a0c51f2f588a52ac0cbb8187
Lyubov <luartal@mail.ru>
2026-09-16 21:34:19 +0300

    fix: mocks only in dev

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './global.css';
 import App from './app';
 
 async function enableMocking() {
+	if (!import.meta.env.DEV) return;
 	const { worker } = await import('./mocks/browser');
 	return worker.start({ onUnhandledRequest: 'bypass' });
 }
